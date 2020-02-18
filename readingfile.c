d86baf5fc2a353d4262d1b30c083c90116b2f412
Zentilt <1901842@sit.singaporetech.edu.sg>
2020-02-18 10:36:52 +0800

    --

--- readingfile.c
+++ readingfile.c
@@ -4,11 +4,11 @@
 
 #define FILESIZE 100
 FILE *fp;
-struct data{
+typedef struct{
       char func[6];//to read add, read, delete based on first letter
       int filename;
-      int data[10];
-};
+      int data[FILESIZE];
+}data;
 
 int readFile();
 void main()
@@ -37,10 +37,9 @@ int readFile(){
 
    char file_name[25];
    int i = 0;
-   struct data d[10];//basically able to read 10 lines so got 10 different set of data
+   data d[10];//basically able to read 10 lines so got 10 different set of data
    char *token;
    char buff[1024];
-   
 
    printf("Enter name of a file you wish to see\n");
    gets(file_name);
@@ -52,7 +51,7 @@ int readFile(){
       perror("Error while opening the file.\n");//if file does not exist
       exit(EXIT_FAILURE);
    }
-   while(fgets(buff,1024,fp)){
+   while(fgets(buff,sizeof(buff),fp)){
       int j = 0;
       token = strtok(buff,",");
       strcpy(d[i].func,token);
@@ -61,22 +60,23 @@ int readFile(){
       token = strtok(NULL,",");
       while(token != NULL){   
          d[i].data[j]= atoi(token);
+         printf("%d,",d[i].data[j]);//so they do scan in the values
          token = strtok(NULL,",");
          j++;
       }
       i++;
    } 
-   for(int c= 0; c < i;c++){
-      printf("%s %d\n",d[c].func,d[c].filename);
-      /*int size = 0;
-      int *ptr = d[c].data;
 
-      while (*ptr !=0) {
-         size++;
+   for(int c= 0; c < i;c++){
+      printf("\nFunction: %s Filename: %d Data:",d[c].func,d[c].filename);
+      int *ptr = d[c].data;
+      int size = 0;
+      while(*ptr !=0){
          *ptr++;
-      }*/
+         size++;
+      }
       for(int k = 0; k < 2; k++){//2 instead of size idk le la i fked up
-         printf("--%d--",d[c].data[k]);
+         printf("%d,",d[c].data[k]);
       }
    }
    
